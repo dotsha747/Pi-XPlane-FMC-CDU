be618fa8fa1150662b87706a307a7cf1a703e71a
Lefteris Kororos <lkororos@gmail.com>
2022-12-28 09:51:51 +0200

    Implemented simulated version on x86 platform

--- src/piXPlaneFMCCDU/PiXPlaneFMCCDU.cpp
+++ src/piXPlaneFMCCDU/PiXPlaneFMCCDU.cpp
@@ -29,7 +29,12 @@
 #include "FMCManager.h"
 
 #include "Screen.h"
-#include "KeypadScanner.h"
+#include "platform/KeypadScanner.h"
+// #if defined(__x86_64__)
+// #include "KeyboardScanner.h"
+// #else
+// #include "KeypadScanner.h"
+// #endif
 #include "RotaryEncScanner.h"
 
 //Screen dimension constants
